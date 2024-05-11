78eca3e035d09b402b693b3241f6161979f821ec
LazarCabarkapa <162819425+LazarCabarkapa@users.noreply.github.com>
2024-05-11 16:45:08 +0200

    a

--- Lazar/script.js
+++ Lazar/script.js
@@ -1,67 +1,49 @@
-let signs = ['+', '-', '*']
-let container_main = document.querySelector('.main')
-let container_start = document.querySelector('.start')
-let container_start_h3 = container_start.querySelector('h3')
-let question_field = document.querySelector('.question')
-let answer_buttons = document.querySelectorAll('.answer')
-let start_button = document.querySelector('.start-btn')
+document.getElementById('saberi').addEventListener('click', function() {
+    izracunaj('+');
+});
 
+document.getElementById('oduzmi').addEventListener('click', function() {
+    izracunaj('-');
+});
 
-let cookie = false
-let cookies = document.cookie.split('; ')
-for (let i = 0; i < cookies.length; i += 1) {
-    if (cookies[i].split('=')[0] == 'numbers_high_score') {
-        cookie = cookies[i].split('=')[1]
-        break
-    }
-}
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    while (currentIndex != 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
-  }
-  if (cookie) {
-    let data = cookie.split('/')
-    container_start_h3.innerHTML = `<h3>Last time you gave ${data[1]} correct answers out of ${data[0]}. Accuracy is ${Math.round(data[1] * 100 / data[0])}%.</h3>`
-}
+document.getElementById('pomnozi').addEventListener('click', function() {
+    izracunaj('*');
+});
 
+document.getElementById('podijeli').addEventListener('click', function() {
+    izracunaj('/');
+});
 
+function izracunaj(operator) { // do sad smo koristili let za deklarisanje promjenljive
+    var broj1 = parseFloat(document.getElementById('broj1').value);
+    var broj2 = parseFloat(document.getElementById('broj2').value);
+    var rezultatElement = document.getElementById('rezultat');
+    var rezultat;
 
+    switch (operator) {
+        case '+':
+            rezultat = broj1 + broj2;
+            break;
+        case '-':
+            rezultat = broj1 - broj2;
+            break;
+        case '*':
+            rezultat = broj1 * broj2;
+            break;
+        case '/':
+            if(broj2==0){
+                rezultat = "Ne moze se dijeliti sa nulom!"
+            }
+            else{
+                rezultat = broj1/broj2;
 
-function randint(min, max) {
-    return Math.round(Math.random() * (max - min) + min)
-}
-
-
-function getRandomSign() {
-    return signs[randint(0, 2)]
-}
-class Question {
-    constructor() {
-        let a = randint(1, 30)
-        let b = randint(1, 30)
-        let sign = getRandomSign()
-        this.question = `${a} ${sign} ${b}`
-        if (sign == '+') { this.correct = a + b }
-        else if (sign == '-') { this.correct = a - b }
-        else if (sign == '*') { this.correct = a * b }
-        this.answers = [
-            randint(this.correct - 20, this.correct - 1),
-            randint(this.correct - 20, this.correct - 1),
-            this.correct,
-            randint(this.correct + 1, this.correct + 20),
-            randint(this.correct + 1, this.correct + 20),
-        ]
-        shuffle(this.answers);
+            }
+            break;
+        default:
+            rezultat = "Nepoznat operator";
     }
 
+<<<<<<< Updated upstream
 
     display () {
         question_field.innerHTML = this.question
@@ -144,3 +126,7 @@ function calculate() {
     }
 }
 
+=======
+    rezultatElement.textContent = rezultat;
+}
+>>>>>>> Stashed changes
